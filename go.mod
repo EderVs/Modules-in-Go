b585a8987a5cc2197a472edc33d283aa7e7e4b8a
edervs <eedervs.97@gmail.com>
2021-04-14 14:15:17 -0500

    Version 1

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/edervs/Modules-in-Go
 
 go 1.15
 
-require github.com/edervs/Modules-in-Go/submodule v1.0.0 // indirect
+require (
+	github.com/edervs/Modules-in-Go/submodule v1.0.0 // indirect
+	github.com/edervs/Modules-in-Go/submodule/v2 v2.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/edervs/Modules-in-Go/submodule"
+import (
+    "github.com/edervs/Modules-in-Go/submodule"
+
+    submodulev2 "github.com/edervs/Modules-in-Go/submodule/v2"
+)
 
 func main() {
     submodule.PrintSubmodule()
+    submodulev2.PrintSubmodule("Person")
 }
 
