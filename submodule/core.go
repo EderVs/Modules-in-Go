bbff1b952ad374a5f3cdc9dd9a73c265d74c2e49
edervs <eedervs.97@gmail.com>
2021-04-15 12:42:43 -0500

    adding v2.0.2 submodule

--- submodule/core.go
+++ submodule/core.go
@@ -1,3 +1,3 @@
 package submodule
 
-const version = "v2.0.1"
+const version = "v2.0.2"
--- submodule/utils.go
+++ submodule/utils.go
@@ -3,5 +3,5 @@ package submodule
 import "fmt"
 
 func PrintSubmodule(name string) {
-    fmt.Printf("Hello, %s. This is the submodule in the version %s\n", name, version)
+    fmt.Printf("Hello, %s! This is the submodule in the version %s\n", name, version)
 }
