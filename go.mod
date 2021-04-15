eb63f54542fba582dd00551302a6b2a67d6291e7
edervs <eedervs.97@gmail.com>
2021-04-15 12:50:50 -0500

    Adding all versions

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.15
 
 require (
 	github.com/edervs/Modules-in-Go/submodule v1.0.1
-	github.com/edervs/Modules-in-Go/submodule/v2 v2.0.1
+	github.com/edervs/Modules-in-Go/submodule/v2 v2.0.2
+	github.com/edervs/Modules-in-Go/submodule/v3 v3.0.0 // indirect
+	github.com/edervs/Modules-in-Go/submodule2 v1.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
     "github.com/edervs/Modules-in-Go/submodule"
+    "github.com/edervs/Modules-in-Go/submodule2"
 
     submodulev2 "github.com/edervs/Modules-in-Go/submodule/v2"
+    submodulev3 "github.com/edervs/Modules-in-Go/submodule/v3"
 )
 
 func main() {
     submodule.PrintSubmodule()
     submodulev2.PrintSubmodule("Person")
+    submodulev3.PrintSubmodule("Person", 18)
+    submodule2.Welcome("Person")
 }
 
